e4cdc0fc7f3b40215a41bc6d50dd9cf1c8e4fd35
Zhi Hao <Zhi_Hao_Lim@Dell.com>
2019-08-29 16:43:33 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
